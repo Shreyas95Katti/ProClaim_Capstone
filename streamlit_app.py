4cc414d6f91552606b9163e7d1dd625cc8ed7298
SHREYAS KATTI <79981786+Shreyas95Katti@users.noreply.github.com>
2025-01-24 15:13:23 +0530

    Streamlit Compatibility Check

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ warnings.filterwarnings('ignore')
 CLASS_INDEX = None
 CLASS_INDEX_PATH = 'https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json'
 
-model_urls = ["https://drive.google.com/uc?id=1xKyczvMpIVCx4XB72lH1sQlnvN_6n_Q8", "https://drive.google.com/uc?id=17DvZPqdMaku7a_wrw2l7eq9qqTVlxTmG", "https://drive.google.com/uc?id=1VJe4GlOKjf9yj-QMuHyV7D5oFZCqLqGA"]
-model_file_paths = ["Vehicle_Damaged_Decision_Model.h5", "Vehicle_Damage_Part_YOLOv8_FineTuned_Model.pt", "Vehicle_Damage_Type_YOLOv8_FineTuned_Model.pt"]
+model_urls = ["https://drive.google.com/uc?id=1xKyczvMpIVCx4XB72lH1sQlnvN_6n_Q8", "https://drive.google.com/uc?id=17DvZPqdMaku7a_wrw2l7eq9qqTVlxTmG"]#, "https://drive.google.com/uc?id=1VJe4GlOKjf9yj-QMuHyV7D5oFZCqLqGA"]
+model_file_paths = ["Vehicle_Damaged_Decision_Model.h5", "Vehicle_Damage_Part_YOLOv8_FineTuned_Model.pt"]#, "Vehicle_Damage_Type_YOLOv8_FineTuned_Model.pt"]
 
 def download_models(model_url, model_file_path):
     if not os.path.exists(model_file_path):
@@ -131,12 +131,12 @@ if uploaded_file is not None:
         if pipe1(img_224, model1):
             if pipe2(img_256, model2):
                 object_msg, obj_image_path = object_detect(img_path)
-                segmentation_msg, seg_image_path = segment(img_path)
+                # segmentation_msg, seg_image_path = segment(img_path)
 
                 st.success("Assessment Completed!")
                 st.image(obj_image_path, caption="Object Detection Result", use_container_width=True)
-                st.write(f"**Segmentation:** {segmentation_msg}")
-                st.image(seg_image_path, caption="Segmentation Result", use_container_width=True)
+                # st.write(f"**Segmentation:** {segmentation_msg}")
+                # st.image(seg_image_path, caption="Segmentation Result", use_container_width=True)
 
             else:
                 st.warning("Damage validation failed. Please upload a clearer image.")
